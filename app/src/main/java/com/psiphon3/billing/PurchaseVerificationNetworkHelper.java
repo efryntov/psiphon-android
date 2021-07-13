0081f09073700397e4c6e0f101b4215be9d507a2
Eugene Fryntov <e.fryntov@psiphon.ca>
2021-07-13 17:07:33 -0400

    Use dev server for purchase verification

--- app/src/main/java/com/psiphon3/billing/PurchaseVerificationNetworkHelper.java
+++ app/src/main/java/com/psiphon3/billing/PurchaseVerificationNetworkHelper.java
@@ -51,8 +51,8 @@ import okhttp3.Response;
 public class PurchaseVerificationNetworkHelper {
 
     private static final int TIMEOUT_SECONDS = 30;
-    private static final String SUBSCRIPTION_VERIFICATION_URL = "https://subscription.psiphon3.com/v2/playstore/subscription";
-    private static final String PSICASH_VERIFICATION_URL = "https://subscription.psiphon3.com/v2/playstore/psicash";
+    private static final String SUBSCRIPTION_VERIFICATION_URL = "https://dev-subscription.psiphon3.com/v2/playstore/subscription";
+    private static final String PSICASH_VERIFICATION_URL = "https://dev-subscription.psiphon3.com/v2/playstore/psicash";
     private static final MediaType JSON = MediaType.parse("application/json");
     private static final String HTTP_USER_AGENT = "Psiphon-Verifier-Android";
     private static final int TRIES_COUNT = 5;
